ca2e5d3adc24d079843e043b2d2687f10b502c4e
Devin Põlluste <devinpolluste@gmail.com>
2021-01-07 21:08:00 +0200

    Sidebar creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
+      {/* Sidebar*/}
+      <Sidebar />
 
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
