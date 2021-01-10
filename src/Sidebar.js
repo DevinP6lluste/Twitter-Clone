c1327964edece19c10f5a7721838ff22791939ca
Devin Põlluste <devinpolluste@gmail.com>
2021-01-10 21:20:52 +0200

    Sidebar Option

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Sidebar.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <TwitterIcon />
+            <SidebarOption />
         </div>
     );
 }
