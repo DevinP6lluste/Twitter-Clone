625f912ec1783e4908c709c01c34a6d714cebd1d
Devin Põlluste <devinpolluste@gmail.com>
2021-01-26 12:20:52 +0200

    Feed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import React from 'react';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
     <div className="app">
-      {/* Sidebar*/}
       <Sidebar />
 
       {/* Feed */}
+      <Feed />
+
       {/* Widgets */}
     </div>
   );
---
+++ src/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Feed() {
+    return (
+        <div>
+            <h2>Home</h2>
+            {/* Header */}
+
+            {/* Tweetbox */}
+
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+        </div>
+    );
+}
+
+export default Feed;
