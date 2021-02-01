e71238dc08038ece44b2e77c2dd1142477372f56
DevinP6lluste <devinpolluste@gmail.com>
2021-02-01 17:58:59 +0200

    Tweetbox

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TweetBox from './TweetBox';
 import "./Feed.css";
 
 function Feed() {
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './TweetBox.css';
+import { Avatar, Button } from '@material-ui/core';
 
 function TweetBox() {
     return (
         <div className="tweetBox">
-            
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar src="https://pbs.twimg.com/profile_images/1343633760542208002/CZ5ETyT__400x400.jpg"/>
+                    <input placegolder="What's happening?" type="text" />
+                    <input placeholder="Enter image URL" type="text" />
+                </div>
+                <Button>Tweet</Button>
+            </form>
         </div>
     );
 }
